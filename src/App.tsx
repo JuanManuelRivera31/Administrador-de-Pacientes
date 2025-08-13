86a88e372c040b9f36814b5035c69b68df5584ed
JuanManuelRivera31 <juanm.rivera3105@gmail.com>
2025-08-13 15:24:44 -0500

    Inicializando proyecto

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl font-bold underline text-center text-red-500'>
+        Hijueputa
+      </h1>
+    </>
+  )
+}
+
+export default App
